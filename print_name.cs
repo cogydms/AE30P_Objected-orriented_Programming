af4ced968a3fe18895e9ecdff896ef55ed7b5938
cogydms <cogydms@dankook.ac.kr>
2025-09-15 18:31:39 +0900

    First commit

---
+++ print_name.cs
@@ -0,0 +1,11 @@
+string name;
+int birthYear, age;
+
+Console.WriteLine("What is your name? ");
+name = Console.ReadLine();
+
+Console.WriteLine("Which year were you born?");
+birthYear = int.Parse(Console.ReadLine()); // 문자를 숫자로 바꾸기
+age = 2025 - birthYear;
+
+Console.WriteLine($"Hello {name}, you are {age} years old");

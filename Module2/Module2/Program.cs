d07c35975367b3f3338040e547b736677174bc69
cogydms <cogydms@dankook.ac.kr>
2025-09-29 21:06:08 +0900

    Add DiceGame and StudentInform projects to Module2 solution

---
+++ Module2/Module2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*using Module2.Classes;
+
+Car car1 = new Car(); 
+// class obj = new invoke class constructor ();
+
+car1.brand = "Honda";
+Console.WriteLine("Brnad: "+ car1.brand);
+Console.WriteLine("Year: "+car1.year);
+
+Car car2 = new ("Audi", 2018, 500);
+Console.WriteLine("Brand : "+ car2.brand);
+Console.WriteLine("Year : "+ car2.mileage);
+
+car.StartEngine();
+Console.WriteLine($"Car age is ");*/
